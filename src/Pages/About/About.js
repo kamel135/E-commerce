2013f9d1cdc84ab2742ba08f0e1d5d654ca96e59
kamel135 <kamel192401@fci.bu.edu.eg>
2022-09-25 03:09:00 +0200

    update

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,13 +1,28 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
+import "./About.css";
 
-const About = ({FooterHandler}) => {
 
+const About = ({ FooterHandler }) => {
   useEffect(() => {
-    return FooterHandler(true)
-  },[FooterHandler])
+    return FooterHandler(true);
+  }, [FooterHandler]);
 
   return (
-    <div>About</div>
-  )
-}
-export default About
+    <div className="About">
+      <div className="container">
+        <div className="About-wapper">
+          <h3>About</h3>
+          <p>
+            A global online fashion and lifestyle retail company, committed to
+            making the beauty of fashion accessible to all. We use on-demand
+            manufacturing technology to connect suppliers to the supply chain,
+            reducing inventory waste and enabling us to offer a diverse range of
+            products at affordable prices to customers around the world. From
+            our global offices, we reach clients in more than 150 countries.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default About;
